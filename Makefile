0347545cd91f8642d7fc769e62ad238584d185d6
joaosilvalopes <jpgdsl@gmail.com>
2020-02-20 19:20:38 +0000

    initial solution

--- Makefile
+++ Makefile
@@ -1,8 +1,14 @@
 in = $p
+out = $(in)
+
+LETTERS = a b c d e f
 
 cpp:
 	g++ -std=c++11 -O2 -Wall $p.cpp -o $p.out
 	./$p.out < $(in).txt
 
 js:
-	node $p.js < $(in).txt
+	node $p.js < $(in).txt | tee ./$(out).out.txt
+
+alljs:
+	$(foreach var,$(LETTERS),make js p=main in=$(var);)
--- main.js
+++ main.js
@@ -4,8 +4,49 @@ const stdin = require('fs')
 	.split(/\s+/)
 	[Symbol.iterator]();
 
-let n = stdin.next().value;
+let nBooks = stdin.next().value;
+let nLibs = stdin.next().value;
+const nDays = stdin.next().value;
 
-while (n--) {}
+const books = [];
 
-console.log(n);
+while (nBooks--) {
+	books.push(+stdin.next().value);
+}
+
+let libs = [];
+
+while (nLibs--) {
+	let nBooks = stdin.next().value;
+	const lib = {
+		signup: stdin.next().value,
+		booksPerDay: stdin.next().value,
+		books: [],
+		scoreSum: 0,
+		i: libs.length
+	};
+
+	while (nBooks--) {
+		const i = stdin.next().value;
+
+		lib.books.push(i);
+		lib.scoreSum += +books[i];
+	}
+
+	lib.books = lib.books.sort((a, b) => books[b] - books[a]);
+
+	lib.score = lib.scoreSum / lib.signup;
+
+	libs.push(lib);
+}
+
+libs = libs.sort((l1, l2) => l2.score - l1.score);
+
+let s = libs.length + '\n';
+
+for (const lib of libs) {
+	s += lib.i + ' ' + lib.books.length + '\n';
+	s += lib.books.join(' ') + '\n';
+}
+
+console.log(s);
